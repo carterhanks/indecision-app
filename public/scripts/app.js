faa3360922035949a158128f8b5ae58c276ba80c
carterhanks <carterhanks@gmail.com>
2021-09-06 12:02:03 -0600

    Used JSX Expressions

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,7 +1,6 @@
 "use strict";
 
 console.log("app.js is running");
-
 //JSX - JavaScript XML (looks like html, but is in fact JSX, and is common found in curly braces).
 
 var template = React.createElement(
@@ -29,26 +28,66 @@ var template = React.createElement(
 	)
 );
 
-var tempmlateTwo = React.createElement(
+// let userName = "Carter Hanks";
+// let userAge = 27;
+// let userLocation = "Utah";
+
+var user = {
+	name: "Carter Hanks",
+	age: 27,
+	location: "Utah"
+};
+
+var templateTwo =
+// <div>
+// 	<h1>{userName.toUpperCase() + "!"}</h1>
+// 	<p>Age: {userAge}</p>
+// 	<p>Location: {userLocation}</p>
+// </div> - SAME AS BELOW:
+React.createElement(
 	"div",
 	null,
 	React.createElement(
 		"h1",
 		null,
-		"Carter Hanks"
+		user.name + "!"
 	),
 	React.createElement(
 		"p",
 		null,
-		"Age: 27"
+		"Age: ",
+		user.age
 	),
 	React.createElement(
 		"p",
 		null,
-		"Location: Utah"
+		"Location: ",
+		user.location
+	)
+);
+
+//TODO - Create app object title/subtitle, use title/subtitle in template, render template
+
+var app = {
+	title: "Indecision App",
+	subtitle: "Make up your mind."
+};
+
+var templateThree = React.createElement(
+	"div",
+	null,
+	React.createElement(
+		"h1",
+		null,
+		app.title
+	),
+	React.createElement(
+		"p",
+		null,
+		app.subtitle
 	)
 );
 
 var appRoot = document.getElementById("app");
 
-ReactDOM.render(tempmlateTwo, appRoot);
+ReactDOM.render(templateThree, appRoot);
--- src/app.js
+++ src/app.js
@@ -1,5 +1,4 @@
 console.log("app.js is running");
-
 //JSX - JavaScript XML (looks like html, but is in fact JSX, and is common found in curly braces).
 
 const template = (
@@ -11,14 +10,43 @@ const template = (
 	</div>
 );
 
-const tempmlateTwo = (
+// let userName = "Carter Hanks";
+// let userAge = 27;
+// let userLocation = "Utah";
+
+const user = {
+	name: "Carter Hanks",
+	age: 27,
+	location: "Utah"
+};
+
+const templateTwo = (
+	// <div>
+	// 	<h1>{userName.toUpperCase() + "!"}</h1>
+	// 	<p>Age: {userAge}</p>
+	// 	<p>Location: {userLocation}</p>
+	// </div> - SAME AS BELOW:
 	<div>
-		<h1>Carter Hanks</h1>
-		<p>Age: 27</p>
-		<p>Location: Utah</p>
+		<h1>{user.name + "!"}</h1>
+		<p>Age: {user.age}</p>
+		<p>Location: {user.location}</p>
+	</div>
+);
+
+//TODO - Create app object title/subtitle, use title/subtitle in template, render template
+
+const app = {
+	title: "Indecision App",
+	subtitle: "Make up your mind."
+};
+
+const templateThree = (
+	<div>
+		<h1>{app.title}</h1>
+		<p>{app.subtitle}</p>
 	</div>
 );
 
 const appRoot = document.getElementById("app");
 
-ReactDOM.render(tempmlateTwo, appRoot);
+ReactDOM.render(templateThree, appRoot);
