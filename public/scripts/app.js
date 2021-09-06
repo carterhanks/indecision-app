dfe843d0b98bbd12f86e4afc6a1f6019b11b53f4
carterhanks <carterhanks@gmail.com>
2021-09-06 11:43:43 -0600

    RStep one Complete

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -5,10 +5,50 @@ console.log("app.js is running");
 //JSX - JavaScript XML (looks like html, but is in fact JSX, and is common found in curly braces).
 
 var template = React.createElement(
-  "h1",
-  null,
-  "Indecision App"
+	"div",
+	null,
+	React.createElement(
+		"h1",
+		null,
+		"Indecision App"
+	),
+	" ",
+	React.createElement(
+		"p",
+		null,
+		"This is some info"
+	),
+	React.createElement(
+		"ol",
+		null,
+		React.createElement(
+			"li",
+			null,
+			"Item One"
+		)
+	)
 );
+
+var tempmlateTwo = React.createElement(
+	"div",
+	null,
+	React.createElement(
+		"h1",
+		null,
+		"Carter Hanks"
+	),
+	React.createElement(
+		"p",
+		null,
+		"Age: 27"
+	),
+	React.createElement(
+		"p",
+		null,
+		"Location: Utah"
+	)
+);
+
 var appRoot = document.getElementById("app");
 
-ReactDOM.render(template, appRoot);
+ReactDOM.render(tempmlateTwo, appRoot);
--- src/app.js
+++ src/app.js
@@ -2,7 +2,23 @@ console.log("app.js is running");
 
 //JSX - JavaScript XML (looks like html, but is in fact JSX, and is common found in curly braces).
 
-const template = <h1>Indecision App</h1>;
+const template = (
+	<div>
+		<h1>Indecision App</h1> <p>This is some info</p>
+		<ol>
+			<li>Item One</li>
+		</ol>
+	</div>
+);
+
+const tempmlateTwo = (
+	<div>
+		<h1>Carter Hanks</h1>
+		<p>Age: 27</p>
+		<p>Location: Utah</p>
+	</div>
+);
+
 const appRoot = document.getElementById("app");
 
-ReactDOM.render(template, appRoot);
+ReactDOM.render(tempmlateTwo, appRoot);
